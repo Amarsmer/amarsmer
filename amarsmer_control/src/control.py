d6ea10ca13f596ec148a631eb867b901ddccf96f
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2025-04-07 15:57:40 +0200

    urdf parser example

--- amarsmer_control/src/control.py
+++ amarsmer_control/src/control.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
-from rclpy.node import Node
+from rclpy.node import Node, QoSProfile
+from rclpy.qos import QoSDurabilityPolicy
 import rclpy
 from amarsmer_control import ROV
 from math import cos
+from urdf_parser_py import urdf
+from std_msgs.msg import String
 
 
 class Controller(Node):
@@ -15,14 +18,37 @@ class Controller(Node):
         joints = [f'thruster{i}_steering' for i in range(1,5)]
         self.rov = ROV(self, thrusters, joints)
 
+        self.robot = None
+        self.robot_sub = self.create_subscription(String, 'robot_description',
+                                                  self.read_model,
+                                                  QoSProfile(depth=1,
+                                                    durability=QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL))
+
         self.timer = self.create_timer(0.1, self.move)
+
+
+    def read_model(self, msg):
+
+        self.robot = urdf.Robot.from_xml_string(msg.data)
+
+        print(len(self.robot.joints), 'joints')
+        print(len(self.robot.links), 'links')
+
+        j1 = self.robot.joints[0]
+        print(j1.parent, j1.child, j1.axis)
+
+        l1 = self.robot.links[0]
+        print('mass:',l1.inertial.mass)
+
+
+
 
     def get_time(self):
         s,ns = self.get_clock().now().seconds_nanoseconds()
         return s + ns*1e-9
 
     def move(self):
-        if not self.rov.ready():
+        if not self.rov.ready() or self.robot is None:
             return
 
         t = self.get_time()
