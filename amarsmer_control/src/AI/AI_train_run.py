9ae8a04d87911087f6b399c675f59d7255b46f2b
Amarsmer <yannick.noe@enib.fr>
2025-09-25 16:51:13 +0200

    Small code cleanup, translated comments to english

--- amarsmer_control/src/AI/AI_train_run.py
+++ amarsmer_control/src/AI/AI_train_run.py
@@ -24,7 +24,7 @@ import functions as f
 from amarsmer_control import ROV
 from backprop import NN
 from online_training import PyTorchOnlineTrainer
-from monitoring import RobotMonitorAdapter
+# from monitoring import RobotMonitorAdapter
 
 # Training specific librairies
 import torch
@@ -46,27 +46,25 @@ class Controller(Node):
 
         self.rov = ROV(self, thrust_visual = True)
 
+        # Publisher and subscribers
         self.odom_subscriber = self.create_subscription(Odometry, '/amarsmer/odom', self.odom_callback, 10)
-        self.str_input_subscriber = self.create_subscription(String, '/amarsmer/input_str', self.str_input_callback, 10)
+        self.str_input_subscriber = self.create_subscription(String, '/amarsmer/input_str', self.str_input_callback, 10) # Used to interact with the training process
         self.pose_arrow_publisher = self.create_publisher(Marker, "/pose_arrow", 10)
 
         self.data_publisher = self.create_publisher(Float32MultiArray, "/monitoring_data", 10)
         self.network_publisher = self.create_publisher(Float32MultiArray, "/network_data", 10)
 
-        self.future = None # Used for client requests
-
         self.timer = self.create_timer(0.1, self.run)
 
         ## Initiating variables
-
-        # Définir les dimensions du monde pour le monitoring
-        self.WORLD_BOUNDS = (-10, 10, -10, 10)  # x_min, x_max, y_min, y_max
-
-        # Configuration de base
+        # Network parameters
         HL_size = 100
         input_size = 6
         output_size = 2
+        learning_rate = 5e-4
+        momentum = 0.2
 
+        # Weighting matrices
         self.Q_weight = np.diag([50, # x
                                  50, # y 
                                  40, # psi
@@ -86,9 +84,9 @@ class Controller(Node):
         self.trainer = None
         self.training_initiated = False
 
-        # Initiate monitoring data
+        # Initiate monitoring data, both stored as .npy and published on a topic
         self.monitoring = []
-        self.monitoring.append(['x','y','psi','x_d','y_d','psi_d','u1','u2','t'])
+        self.monitoring.append(['x','y','psi','x_d','y_d','psi_d','u1','u2','t']) # Naming the variables in the first row, useful as is and even more so if data points change between version
 
         self.date = datetime.today().strftime('%Y_%m_%d-%H_%M_%S')
 
@@ -106,7 +104,7 @@ class Controller(Node):
         self.input_string = msg.data
 
     def updateRobotState(self):
-        # # Extract position and speed as column vectors
+        # Extract position and speed as column vectors
         position = np.array([self.rov.current_pose[x] for x in [0, 1, 5]]).reshape(-1, 1)
         speed = np.array([self.rov.current_twist[x] for x in [0, 1, 5]]).reshape(-1, 1)
 
@@ -126,134 +124,134 @@ class Controller(Node):
         target_pose = f.make_pose(target)
         f.create_pose_marker(target_pose, self.pose_arrow_publisher)
 
-        if not self.training_initiated: # This code used to be in a while loop and requires adjustements to work as a ROS2 node
-            self.training_initiated = True
 
-            self.t0 = self.get_time() # Initial time for data collection
+    # Initialize
+    if not self.training_initiated: # This code used to be in a while loop and requires adjustements to work as a ROS2 node
+        self.training_initiated = True
 
-            # Weight loading
-            if self.get_parameter('load_weights').get_parameter_value().bool_value:
-                with open('last_w_torch.json') as fp:
-                    json_obj = json.load(fp)
-                self.network.load_weights_from_json(json_obj, HL_size)
-                
-            # Initialize trainer
-            self.trainer = PyTorchOnlineTrainer(self.rov, self.network, None, self.Q_weight, self.R_weight)
+        self.t0 = self.get_time() # Initial time for data collection
 
-            train = self.get_parameter('train').get_parameter_value().bool_value #Boolean
-
-            # Main training loop, currently runs only once
-            continue_running = True
-            session_count = 0
-            session_count += 1
-
-            self.updateRobotState() # The trainer thread requires manual update of the robot's state
-            self.trainer.updateTarget(target) # To be used for trajectory tracking
-
-            self.get_logger().info(f"\n⚙️ Starting training session #{session_count}")
-
-            self.training_thread = threading.Thread(target=self.trainer.train, args=(target,)) # Start training process on a separate thread
-            self.trainer.running = True
-            self.training_thread.start()
+        # Weight loading
+        if self.get_parameter('load_weights').get_parameter_value().bool_value:
+            with open('last_w_torch.json') as fp:
+                json_obj = json.load(fp)
+            self.network.load_weights_from_json(json_obj, HL_size)
             
-            #TODO: add flexibility to run multiple training sessions
-            """
-            input_string = self.get_parameter('input_string').value
-            try:
-                if input_string != '':
-                    # self.input_string = ''
-                    self.set_parameters([Parameter('input_string', Parameter.Type.STRING, '')])
-                    # input("Press Enter to stop the current training")
-                    self.trainer.running = False
-                
-                training_thread.join(timeout=5)
-                if training_thread.is_alive():
-                    print("⚠️ Training thread did not finish in time, continuing anyway")
-                    
-            except KeyboardInterrupt:
-                print("\nKeyboard interrupt detected. Stopping training...")
+        # Initialize trainer
+        self.trainer = PyTorchOnlineTrainer(self.rov, self.network, learning_rate, momentum, self.Q_weight, self.R_weight)
+
+        train = self.get_parameter('train').get_parameter_value().bool_value #Boolean
+
+        # Main training loop, currently runs only once
+        continue_running = True
+        session_count = 0
+        session_count += 1
+
+        self.updateRobotState() # The trainer thread requires manual update of the robot's pose and twist
+        self.trainer.updateTarget(target) # To be used for trajectory tracking
+
+        self.get_logger().info(f"\n⚙️ Starting training session #{session_count}")
+
+        self.training_thread = threading.Thread(target=self.trainer.train, args=(target,)) # Start training process on a separate thread
+        self.trainer.running = True
+        self.training_thread.start()
+        
+        #TODO: add flexibility to run multiple training sessions
+        """
+        input_string = self.get_parameter('input_string').value
+        try:
+            if input_string != '':
+                # self.input_string = ''
+                self.set_parameters([Parameter('input_string', Parameter.Type.STRING, '')])
+                # input("Press Enter to stop the current training")
                 self.trainer.running = False
-                training_thread.join(timeout=5)
-
-            if display_curves:
-                monitor.save_results(f"session_{session_count}_{time.strftime('%Y%m%d_%H%M%S')}")
-
-            # choice = ''
-            self.get_logger().info("Do you want to continue? (y/n) --> ")
-            while input_string.lower() not in ['y', 'n']:
-                input_string = self.get_parameter('input_string').value
-                self.get_logger().info(f"stored variable: {input_string}")
-                # choice = input("Do you want to continue? (y/n) --> ")
-
-            if input_string.lower() == 'y':
-                choice_learning = ''
-                while choice_learning.lower() not in ['y', 'n']:
-                    choice_learning = input('Do you want to learn? (y/n) --> ')
+            
+            training_thread.join(timeout=5)
+            if training_thread.is_alive():
+                print("⚠️ Training thread did not finish in time, continuing anyway")
                 
-                self.trainer.training = (choice_learning.lower() == 'y')
-                
-                # target_input = input("Move the robot to the initial point and enter the new target : x y radian --> ")
-                # target = [float(x) for x in target_input.split()]
-                # if len(target) != 3:
-                #     raise ValueError("Need exactly 3 values")
-            
-            else:
-                continue_running = False
-            """
-
-        self.updateRobotState()
-
-        ### Save data for monitoring
-        if self.trainer.command_set: # Make sure the training has started
-            x_m = self.rov.current_pose[0]
-            y_m = self.rov.current_pose[1]
-            psi_m = self.rov.current_pose[5]
-
-            x_d_m = target[0]
-            y_d_m = target[1]
-            psi_d_m = target[2]
-
-            u = self.trainer.command
-
-            t = self.get_time() - self.t0
-
-            data_array = [x_m, y_m, psi_m, x_d_m, y_d_m , psi_d_m, u[0],u[1], t]
-
-            self.monitoring.append(data_array)
-
-            publisher_msg = Float32MultiArray()
-            publisher_msg.data = data_array
-            self.data_publisher.publish(publisher_msg)
-
-            publisher_msg = Float32MultiArray()
-            publisher_msg.data = self.trainer.input_display
-            self.network_publisher.publish(publisher_msg)
-            # self.get_logger().info(f'Publishing: {msg.data}')
-            
-            
-            # Info 
-            # self.get_logger().info(f"Grad: {self.trainer.gradient_flag}") # Print gradient for debugging purposes
-            # self.get_logger().info(f"\n Internal state: {self.trainer.state}")
-            # self.get_logger().info(f"\n Internal error: {self.trainer.error}") 
-            # self.get_logger().info(f"\n Train state: {self.trainer.state_train_display}") 
-            # self.get_logger().info(f"\n Train error: {self.trainer.error_display}")
-            # self.get_logger().info(f"\n Network input: {self.trainer.input_display}")
-            
-
-        if self.input_string == 'stop': # Stop training session from terminal
-            self.input_string = ''
+        except KeyboardInterrupt:
+            print("\nKeyboard interrupt detected. Stopping training...")
             self.trainer.running = False
-            self.training_thread.join(timeout=5)
+            training_thread.join(timeout=5)
 
-            title = 'data/AI_data/' + self.date +'-AI_data'
-            np.save(title, self.monitoring)
+        if display_curves:
+            monitor.save_results(f"session_{session_count}_{time.strftime('%Y%m%d_%H%M%S')}")
 
-            # Save the weights
-            json_obj = self.network.save_weights_to_json()
-            with open('last_w_torch.json', 'w') as fp:
-                json.dump(json_obj, fp)
+        # choice = ''
+        self.get_logger().info("Do you want to continue? (y/n) --> ")
+        while input_string.lower() not in ['y', 'n']:
+            input_string = self.get_parameter('input_string').value
+            self.get_logger().info(f"stored variable: {input_string}")
+            # choice = input("Do you want to continue? (y/n) --> ")
 
-            self.get_logger().info("Training stopped")
+        if input_string.lower() == 'y':
+            choice_learning = ''
+            while choice_learning.lower() not in ['y', 'n']:
+                choice_learning = input('Do you want to learn? (y/n) --> ')
+            
+            self.trainer.training = (choice_learning.lower() == 'y')
+            
+            # target_input = input("Move the robot to the initial point and enter the new target : x y radian --> ")
+            # target = [float(x) for x in target_input.split()]
+            # if len(target) != 3:
+            #     raise ValueError("Need exactly 3 values")
+        
+        else:
+            continue_running = False
+        """
+
+    self.updateRobotState()
+
+    ### Save data for monitoring
+    if self.trainer.command_set: # Make sure the training has started
+        x_m = self.rov.current_pose[0]
+        y_m = self.rov.current_pose[1]
+        psi_m = self.rov.current_pose[5]
+
+        x_d_m = target[0]
+        y_d_m = target[1]
+        psi_d_m = target[2]
+
+        u = self.trainer.command
+
+        t = self.get_time() - self.t0
+
+        data_array = [x_m, y_m, psi_m, x_d_m, y_d_m , psi_d_m, u[0],u[1], t]
+
+        self.monitoring.append(data_array)
+
+        publisher_msg = Float32MultiArray()
+        publisher_msg.data = data_array
+        self.data_publisher.publish(publisher_msg)
+
+        publisher_msg = Float32MultiArray()
+        publisher_msg.data = self.trainer.input_display
+        self.network_publisher.publish(publisher_msg)
+        
+        # Debug info
+        # self.get_logger().info(f"Grad: {self.trainer.gradient_flag}") 
+        # self.get_logger().info(f"\n Internal state: {self.trainer.state}")
+        # self.get_logger().info(f"\n Internal error: {self.trainer.error}") 
+        # self.get_logger().info(f"\n Train state: {self.trainer.state_train_display}") 
+        # self.get_logger().info(f"\n Train error: {self.trainer.error_display}")
+        # self.get_logger().info(f"\n Network input: {self.trainer.input_display}")
+        
+
+    if self.input_string == 'stop': # Stop training session from terminal
+        self.input_string = ''
+        self.trainer.running = False
+        self.training_thread.join(timeout=5)
+
+        title = 'data/AI_data/' + self.date +'-AI_data'
+        np.save(title, self.monitoring)
+
+        # Save the weights
+        json_obj = self.network.save_weights_to_json()
+        with open('last_w_torch.json', 'w') as fp:
+            json.dump(json_obj, fp)
+
+        self.get_logger().info("Training stopped")
 
 rclpy.init()
 node = Controller()
--- amarsmer_control/src/AI/online_training.py
+++ amarsmer_control/src/AI/online_training.py
@@ -9,7 +9,7 @@ def theta_s(x, y):
     return math.tanh(10.*x)*math.atan(1.*y)
 
 class PyTorchOnlineTrainer:
-    def __init__(self, robot, nn_model, monitor=None, Q=np.eye(6), R=np.eye(3)):
+    def __init__(self, robot, nn_model, in_learning_rate = 5e-4, in_momentum = 0.2, Q=np.eye(6), R=np.eye(3)):
 
         self.robot = robot
         self.network = nn_model
@@ -18,18 +18,15 @@ class PyTorchOnlineTrainer:
         self.Q = Q
         self.R = R
         
-        # État de l'apprentissage
+        # Training state
         self.running = False
         self.training = False
 
-        self.learning_rate = 5e-4
-        self.optimizer_momentum = 0.2
-        # Création de l'optimiseur (remplace partiellement la logique de backpropagate)
+        self.learning_rate = in_learning_rate
+        self.optimizer_momentum = in_momentum
+
+        # Set up optimizer (partially replaces backpropagation)
         self.optimizer = torch.optim.SGD(self.network.parameters(), lr=self.learning_rate, momentum=self.optimizer_momentum)
-        
-        #TODO Add monitor support
-        self.monitor = monitor
-        self.last_gradient = [0, 0] if monitor else None
 
         # Variables init
         self.state = None
@@ -47,7 +44,7 @@ class PyTorchOnlineTrainer:
                       [0.        ,0.],
                      [self.r,-self.r]]) 
 
-        # Coefficients for gradient computation (removes unnecessary temp variable attribution)
+        # Coefficients for gradient computation (removes unnecessary temp variable attribution in loop)
         self.m = self.robot.mass
         self.Xudot = self.robot.added_masses[0]
         self.Nrdot = self.robot.added_masses[5]
@@ -61,9 +58,9 @@ class PyTorchOnlineTrainer:
                                  [self.r/(self.Iz - self.Nrdot)   , -self.r/(self.Iz - self.Nrdot)]])
 
         self.command_set = False # Make sure inputs have been computed before recording data
-        self.gradient_flag = None # Used to display gradient in terminal
+        self.gradient_display = None # Used to display gradient in terminal
         self.input_display = None # Used to display network input in terminal
-        self.error_display = None
+        self.error_display = None # Used to display error in terminal
 
     def updateTarget(self, in_target):
         self.target = in_target
@@ -74,57 +71,39 @@ class PyTorchOnlineTrainer:
     def computeError(self):
         # Compute error as a column vector
         error = self.state - np.array(self.target).reshape(-1, 1)
-        
-        # self.error[2] -= theta_s(state[0], state[1])
+        # self.error[2] -= theta_s(state[0], state[1]) # Yaw skew
+
         return error
 
-    def computeNetworkInput(self,error):
-        # error = self.computeError()
-
+    def computeNetworkInput(self, error):
         # Weight matrix used for input normalization
         weight_matrix = np.diag([1/4, 1/4, 1/np.pi, 1, 1, 1])
         network_input = weight_matrix @ error
-        # network_input = error
         
         return network_input.ravel()
 
     def computeGradient(self, delta_t, error):
-        grad_u = np.eye(2) # Not necessary but makes the code closer to the theoretical model
-
         w_tau = self.R @ self.tau
         u = np.linalg.pinv(self.B) @ w_tau
 
         gradxJ = 2 * (self.Q @ error)
         graduJ = 2 * (u)
 
-        grad = delta_t * (self.grad_xk.transpose() @ gradxJ) + grad_u @ graduJ
-        grad = grad.squeeze(-1)
+        grad = delta_t * (self.grad_xk.transpose() @ gradxJ) + graduJ
+        grad = grad.squeeze(-1) # Removes the dimensions of size 1
 
         return grad
 
     def train(self, target):
-        # Update monitor if available
-        if self.monitor:
-            self.monitor.set_target(target)
-        
-        # Boucle d'apprentissage
+        # Training loop
         while self.running:
             # Get initial time for gradient computation later
             start_time = time.time()
 
-            # self.computeError()
-            # error = self.state - np.array(self.target).reshape(-1, 1)
-            # network_input = self.computeNetworkInput(error)
-            # Compute error as a column vector
-            # self.error[2] -= theta_s(state[0], state[1])
-
-            # Weight matrix used for input normalization
-            # weight_matrix = np.diag([1/4, 1/4, 1/np.pi, 1, 1, 1])
-            # network_input = (weight_matrix @ self.error).ravel()
-            # network_input = self.error.ravel()
             error = self.computeError()
             network_input = self.computeNetworkInput(error)
 
+            # Monitoring data for debugging purposes
             self.state_train_display = self.state
             self.error_display = self.error
             self.input_display = network_input
@@ -146,71 +125,46 @@ class PyTorchOnlineTrainer:
             # Evaluate criteria before moving the robot
             self.command = np.array(self.command).reshape(-1, 1)
             self.tau = self.B @ self.command
+
             first_criteria = error.transpose() @ self.Q @ error + self.tau.transpose() @ self.R @ self.tau
 
             # Apply control input
-            input_coefficient = 40
-            # Appliquer les commandes au robot
+            input_coefficient = 40 # The network outputs 1 at max and the thrusters are expected to be able to output 40 newtons
             self.robot.move([self.command[0]*input_coefficient,self.command[1]*input_coefficient,0,0],
                       [0 for i in range(1,5)])
             
             # Wait before evaluating criteria again
             time.sleep(0.050)
 
-            """
-            # self.computeError(target)
-            # Extract position and speed as column vectors
-            position = np.array([self.robot.current_pose[x] for x in [0, 1, 5]]).reshape(-1, 1)
-            speed = np.array([self.robot.current_twist[x] for x in [0, 1, 5]]).reshape(-1, 1)
-
-            # Concatenate vertically (stack columns)
-            state = np.vstack([position, speed])
-            """
-            # Compute error as a column vector
-            # error = self.state - np.array(self.target).reshape(-1, 1)
+            # Update error
             error = self.computeError()
-            # self.error[2] -= theta_s(state[0], state[1])
-
-            # # Weight matrix used for input normalization
-            # weight_matrix = np.diag([1/4, 1/4, 1/np.pi, 1, 1, 1])
-            # # network_input = (weight_matrix @ self.error).ravel()
-            # network_input = self.error.ravel()
            
             second_criteria = error.transpose() @ self.Q @ error + self.tau.transpose() @ self.R @ self.tau
 
             if self.training:
                 delta_t = (time.time() - start_time)
                 grad = self.computeGradient(delta_t, error)             
-
-                # Update monitor with current gradient
-                if self.monitor:
-                    self.monitor.update(
-                        position=position,
-                        wheel_speeds=self.command,
-                        gradient=grad,
-                        cost=first_criteria
-                    )
                 
                 # TODO: adapt learning strategy depending on criteria, currently does the same thing either way
                 # Learning strategy
                 if second_criteria <= first_criteria:
-                    # Effectuer une étape d'apprentissage
+                    # Do a learning step
                     self.optimizer.zero_grad()
                     
-                    # Convertir le gradient
+                    # Convert gradient
                     grad_tensor = torch.tensor(grad, dtype=torch.float32)
                     
-                    # Effectuer une étape d'apprentissage personnalisée
+                    # Do a custom learning step
                     self.manual_backward(input_tensor, grad_tensor, self.learning_rate, self.optimizer_momentum)
                 else:
-                    # Alternative si le critère ne s'améliore pas
-                    # On peut soit ajouter du bruit soit quand même apprendre
+                    # If the criteria does not improve
+                    # Either add noise or learn regardless
                     self.optimizer.zero_grad()
                     
-                    # Convertir le gradient
+                    # Convert gradient
                     grad_tensor = torch.tensor(grad, dtype=torch.float32)
                     
-                    # Effectuer une étape d'apprentissage personnalisée
+                    # Do a custom learning step
                     self.manual_backward(input_tensor, grad_tensor, self.learning_rate, self.optimizer_momentum)
         
         # Stop the robot after learning
@@ -219,27 +173,17 @@ class PyTorchOnlineTrainer:
         self.running = False
     
     def manual_backward(self, inputs, grad_tensor, learning_rate, momentum):
-        """
-        Effectue manuellement une étape de rétropropagation avec le gradient fourni
-        
-        Args:
-            inputs: les entrées du réseau
-            grad_tensor: le gradient du critère par rapport aux sorties
-            learning_rate: le taux d'apprentissage
-            momentum: le facteur de momentum
-        """
-        # Réinitialiser les gradients
+        # Reset gradients
         self.optimizer.zero_grad()
         
-        # Forward pass pour établir le graphe de calcul
+        # Forward pass
         outputs = self.network(inputs)
         
-        # Rétropropager le gradient directement
-        # C'est ici que nous connectons notre gradient externe au graphe PyTorch
+        # Manually backpropagate
+        # Connect externally computed gradient to pytorch
         outputs.backward(gradient=grad_tensor)
         
-        # Mise à jour des poids
+        # Update weights
         for param in self.network.parameters():
             if param.grad is not None:
-                # Mise à jour manuelle avec le momentum si nécessaire
                 param.data.add_(param.grad, alpha=-learning_rate)
